14902c3626d3f2d4e4074f957c2475086d89b780
Dvir Shahala <dvirshahala@Dvirs-MacBook-Air.local>
2021-05-08 15:12:24 +0300

    responsive navbar

--- src/app/components/toolbar/toolbar.component.ts
+++ src/app/components/toolbar/toolbar.component.ts
@@ -1,25 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { MatDialog } from '@angular/material/dialog';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { Category } from 'src/app/models/interfaces';
-import { CatergoryService } from 'src/app/services/catergory.service';
-import { AddCategoryComponent } from '../add-category/add-category.component';
-import { UpdateCategoryComponent } from '../update-category/update-category.component';
-import { ViewDetailsComponent } from '../view-details/view-details.component';
+import { Component, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
+import { BehaviorSubject, Observable } from "rxjs";
+import { Category } from "src/app/models/interfaces";
+import { CatergoryService } from "src/app/services/catergory.service";
+import { AddCategoryComponent } from "../add-category/add-category.component";
+import { UpdateCategoryComponent } from "../update-category/update-category.component";
+import { ViewDetailsComponent } from "../view-details/view-details.component";
 
 @Component({
-  selector: 'app-toolbar',
-  templateUrl: './toolbar.component.html',
-  styleUrls: ['./toolbar.component.scss'],
+  selector: "app-toolbar",
+  templateUrl: "./toolbar.component.html",
+  styleUrls: ["./toolbar.component.scss"],
 })
 export class ToolbarComponent implements OnInit {
   currentCategory: Category;
   categoryName: string;
+  navbarOpen: boolean = false;
 
   constructor(
     public catergoryService: CatergoryService,
     public dialog: MatDialog
   ) {}
+
+  openNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 
   ngOnInit(): void {
     this.catergoryService.getCurrentCategory().subscribe((res: Category) => {
@@ -32,11 +37,11 @@ export class ToolbarComponent implements OnInit {
 
   openAddCategoryDialog(): void {
     const dialogRef = this.dialog.open(AddCategoryComponent, {
-      width: '250px',
+      width: "250px",
     });
 
     dialogRef.afterClosed().subscribe((result: string) => {
-      console.log('The dialog was closed');
+      console.log("The dialog was closed");
       this.categoryName = result;
       console.log(this.categoryName);
     });
@@ -44,21 +49,21 @@ export class ToolbarComponent implements OnInit {
 
   openViewDetailsDialog(): void {
     const dialogRef = this.dialog.open(ViewDetailsComponent, {
-      width: '250px',
+      width: "250px",
     });
 
     dialogRef.afterClosed().subscribe((result: string) => {
-      console.log('The dialog was closed');
+      console.log("The dialog was closed");
     });
   }
 
   openUpdateDialog(): void {
     const dialogRef = this.dialog.open(UpdateCategoryComponent, {
-      width: '300px',
+      width: "300px",
     });
 
     dialogRef.afterClosed().subscribe((result: string) => {
-      console.log('The dialog was closed');
+      console.log("The dialog was closed");
     });
   }
 
